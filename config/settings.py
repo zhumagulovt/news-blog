df552354aeff9e0d65552bbb1e43a22367ade61e
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-13 16:06:12 +0600

    Create new app "users"
    Change name of app in apps.py, from "users" to "news_blog.users".
    Add "news_blog.users" to INSTALLED_APPS in settings.

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # local apps
+    "news_blog.users"
 ]
 
 MIDDLEWARE = [
