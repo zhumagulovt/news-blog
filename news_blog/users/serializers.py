549259c740c523b3b49b778704eecaa24476cc29
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-14 17:24:08 +0600

    Add RegistrationSerializer in users app.
    Made first_name and last_name fields required.
    Add validation for password_confirm
    Add create() method, that creates new User.

---
+++ news_blog/users/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for registration"""
+    # Make first_name and last_name fields required
+    first_name = serializers.CharField(max_length=150)
+    last_name = serializers.CharField(max_length=150)
+
+    password_confirm = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username',
+                  'password', 'password_confirm']
+
+    def validate(self, data):
+        """Check that password and password_confirm are same"""
+        # remove non-model field from data
+        password_confirm = data.pop('password_confirm', None)
+
+        if data['password'] != password_confirm:
+            raise serializers.ValidationError('Password confirmation is wrong')
+
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+
+        return user
