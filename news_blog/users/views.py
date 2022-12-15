8fa1d82b56d3fc26b30dc39ca7cb57d339ea810b
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-15 14:00:39 +0600

    Add view for registration
    This is GenericAPIView with only POST method

--- news_blog/users/views.py
+++ news_blog/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from .serializers import RegistrationSerializer
+
+User = get_user_model()
+
+
+class RegistrationAPIView(GenericAPIView):
+    """
+    View for registration
+    """
+    serializer_class = RegistrationSerializer
+    queryset = User.objects.all()
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.create(serializer.validated_data)
+
+        return Response(
+            "Новый пользователь успешно создан",
+            status=status.HTTP_201_CREATED
+        )
