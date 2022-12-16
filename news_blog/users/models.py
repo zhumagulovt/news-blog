cf5adcdc25e2a9731226a17d741db42b9425e890
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-16 18:09:28 +0600

    Create custom User model
    I found out that it was necessary to add profile picture to User model.
    Then extended AbstractUser and add profile_pictire field, also made first_name and last_name fields required.

--- news_blog/users/models.py
+++ news_blog/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUser(AbstractUser):
+    """
+    Кастомная модель пользователя.
+    Добавил поле для фото профиля.
+    Сделал имя и фамилию обязательными полями.
+    """
+    profile_picture = models.ImageField("Фото профиля", upload_to="profile_pictures/", blank=True)
+    first_name = models.CharField("Имя", max_length=150)
+    last_name = models.CharField("Фамилия", max_length=150)
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
