121e1664dd51ab2579beb930df8a73928074d171
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-16 18:41:20 +0600

    fix media settings

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -28,5 +30,12 @@ urlpatterns = [
     # OpenAPI without UI
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Swagger
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
