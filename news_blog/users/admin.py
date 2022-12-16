dfb82a7af8b281aabd3199d43cee4f35b066805c
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-16 18:16:59 +0600

    add CustomUser to django admin

--- news_blog/users/admin.py
+++ news_blog/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(DjangoUserAdmin):
+    """
+    Админка для кастомного пользователя.
+    При создании нового пользователя добавляет в форму имя и фамилию.
+    """
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
