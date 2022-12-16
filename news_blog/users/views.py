80db363d98f5fca2051838910957517d9b06bc0f
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-16 17:19:42 +0600

    add full schema description.

--- news_blog/users/views.py
+++ news_blog/users/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
+from drf_spectacular.utils import extend_schema, OpenApiResponse
+
 from .serializers import RegistrationSerializer
 
 User = get_user_model()
@@ -11,17 +13,23 @@ User = get_user_model()
 
 class RegistrationAPIView(GenericAPIView):
     """
-    View for registration
+    Регистрация по полям: Имя, Фамилия, Логин, Пароль, Подтверждение пароля.
     """
     serializer_class = RegistrationSerializer
-    queryset = User.objects.all()
+    authentication_classes = ()
 
+    # сообщение в response
+    success_message = 'Новый пользователь успешно создан'
+
+    @extend_schema(
+        responses={201: OpenApiResponse(description=success_message)}
+    )
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.create(serializer.validated_data)
 
         return Response(
-            "Новый пользователь успешно создан",
+            self.success_message,
             status=status.HTTP_201_CREATED
         )
