48a8b64be89803286a0ee8ecc2b55e0b3de48b0a
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-16 18:14:15 +0600

    set my CustmUser as default auth.User

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
     "drf_spectacular"
 ]
 
+# User model
+AUTH_USER_MODEL = "users.CustomUser"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
