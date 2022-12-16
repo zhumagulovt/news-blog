219ddef0e64065cebc3a20e6a707854b9603e567
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-16 16:22:21 +0600

    Add endpoints of simplejwt to users.urls

--- news_blog/users/urls.py
+++ news_blog/users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from . import views
 
 urlpatterns = [
-    path('registration/', views.RegistrationAPIView.as_view())
+    path('registration/', views.RegistrationAPIView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
