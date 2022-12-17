727fbf242662dc0b9da2d607772cf9957d73a81c
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-18 00:59:20 +0600

    add UserViewSet to urls

--- news_blog/users/urls.py
+++ news_blog/users/urls.py
@@ -6,9 +6,24 @@ from rest_framework_simplejwt.views import (
 )
 
 from . import views
+from .viewsets import UserViewSet
+
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+
+delete_profile_pic = UserViewSet.as_view({
+    'delete': 'delete_profile_picture'
+})
 
 urlpatterns = [
     path("registration/", views.RegistrationAPIView.as_view()),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("profile/", user_detail),
+    path("profile/picture/", delete_profile_pic)
 ]
