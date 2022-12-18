d02cbd8dcedc93286bda397612f30ef6d4e75ba0
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-19 01:34:04 +0600

    Create admin for Post models

--- news_blog/posts/admin.py
+++ news_blog/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at')
+    list_filter = ('title', 'created_at')
+    search_fields = ('title',)
