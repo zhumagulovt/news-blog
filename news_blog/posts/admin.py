c30fbb77935a7d05ff52d92474f821d4c60102b7
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-20 15:55:11 +0600

    create ModelAdmin for Category model

--- news_blog/posts/admin.py
+++ news_blog/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Category
 
 
 @admin.register(Post)
@@ -8,3 +8,9 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'category', 'created_at')
     list_filter = ('title', 'category', 'created_at')
     search_fields = ('title',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
