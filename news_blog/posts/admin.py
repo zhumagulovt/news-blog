5c7e4ef363feafefdb39240db90a28c47b2b00e9
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-20 15:53:14 +0600

    Add category field to PostAdmin

--- news_blog/posts/admin.py
+++ news_blog/posts/admin.py
@@ -5,6 +5,6 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'created_at')
-    list_filter = ('title', 'created_at')
+    list_display = ('id', 'title', 'author', 'category', 'created_at')
+    list_filter = ('title', 'category', 'created_at')
     search_fields = ('title',)
