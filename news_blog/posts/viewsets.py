c563cf8c75ef77a8262ef9c176f6a0dcf134899c
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-12-20 23:38:04 +0600

    Create ViewSet for Post model

---
+++ news_blog/posts/viewsets.py
@@ -0,0 +1,38 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from drf_spectacular.utils import extend_schema_view, extend_schema
+
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+@extend_schema_view(
+    list=extend_schema(
+        description='Получить все посты'
+    ),
+    create=extend_schema(
+        description='Создать новый пост'
+    ),
+    retrieve=extend_schema(
+        description='Получить данные о посте'
+    ),
+    partial_update=extend_schema(
+        description='Частично изменить пост'
+    ),
+    update=extend_schema(
+        description='Полностью изменить пост'
+    ),
+    destroy=extend_schema(
+        description='Удалить пост'
+    )
+)
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def perform_create(self, serializer):
+        """При сохранении установить текущего пользователя как автора поста"""
+        serializer.save(author=self.request.user)
