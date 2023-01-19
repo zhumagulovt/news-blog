85316a9f7d52a0dab6ce0124179991740c6a89ba
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2023-01-19 12:51:29 +0600

    remove unused imports

--- news_blog/posts/viewsets.py
+++ news_blog/posts/viewsets.py
@@ -8,11 +8,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
 
-from drf_spectacular.utils import (
-    extend_schema_view,
-    extend_schema,
-    OpenApiExample,
-)
+from drf_spectacular.utils import extend_schema
 
 from django_filters.rest_framework import DjangoFilterBackend
 
